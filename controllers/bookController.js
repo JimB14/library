ea96a895b89e34589f70c79b6dd3a9cf7ef92675
Jim <jim.burns14@gmail.com>
2017-09-28 16:32:42 -0400

    testing

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,10 +8,15 @@ var BookInstance = require('../models/bookInstance');
 var async = require('async');
 
 
-exports.index = function(req, res) {
+exports.index = function(req, res, next) {
 
-   res.send("Connected!");
-   return;
+   res.render('home/', {
+      title: 'Local Library Home',
+      error: err,
+      data: results,
+      success: false
+   });
+   // return;
 
    // http://caolan.github.io/async/docs.html#parallel
    // parallel - all functions started at the same time, when all completed,
@@ -19,34 +24,35 @@ exports.index = function(req, res) {
    // Note: The callback function from async.parallel() above is a little unusual
    // in that we render the page whether or not there was an error (normally you
    // might use a separate execution path for handling the display of errors).
-   async.parallel({
-      book_count: function(callback){
-         Book.count(callback);
-      },
-      book_instance_count: function(callback){
-         BookInstance.count(callback);
-      },
-      book_instance_available_count: function(callback){
-         BookInstance.count({status: 'Available'}, callback);
-      },
-      author_count: function(callback){
-         Author.count(callback);
-      },
-      genre_count: function(callback){
-         Genre.count(callback);
-      },
-   }, function(err, results){
-      if(err){
-         console.log(err);
-      } else {
-         res.render('home/', {
-            title: 'Local Library Home',
-            error: err,
-            data: results,
-            success: false
-         });
-      }
-   });
+   // async.parallel({
+   //    book_count: function(callback){
+   //       Book.count(callback);
+   //    },
+   //    book_instance_count: function(callback){
+   //       BookInstance.count(callback);
+   //    },
+   //    book_instance_available_count: function(callback){
+   //       BookInstance.count({status: 'Available'}, callback);
+   //    },
+   //    author_count: function(callback){
+   //       Author.count(callback);
+   //    },
+   //    genre_count: function(callback){
+   //       Genre.count(callback);
+   //    },
+   // }, function(err, results){
+   //    if(err){
+   //       console.log(err);
+   //       return next(err);
+   //    } else {
+   //       res.render('home/', {
+   //          title: 'Local Library Home',
+   //          error: err,
+   //          data: results,
+   //          success: false
+   //       });
+   //    }
+   // });
 };
 
 
